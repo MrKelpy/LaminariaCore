5b8e265a40db84937a45ebebc7dcf592db605488
MrKelpy <alexandresilva.coding@gmail.com>
2021-12-02 11:55:29 +0000

    Added the CSharpLC PathUtils class

---
+++ CSharpLC/LaminariaCore/PathUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace LaminariaCore.LaminariaCore {
+    class PathUtils {
+
+        public static String getCwd() {
+            return Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+        }
+
+
+    }
+}
--- CSharpLC/test/Program.cs
+++ CSharpLC/test/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.IO;
+using LaminariaCore.LaminariaCore;
 
-namespace LaminariaCore
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace LaminariaCore {
+    class Program {
+        static void Main(string[] args) {
+
             Console.WriteLine("Hello World!");
+            String a = Path.Join(PathUtils.getCwd(), "potato");
+            Console.WriteLine(a);
+            Console.ReadKey();
         }
     }
 }
