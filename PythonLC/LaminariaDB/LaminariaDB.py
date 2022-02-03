e409a815dfaa4c83bc6698db78612ecafe8bd14c
Alexandre Silva <alexandresilva.coding@gmail.com>
2022-02-03 23:34:37 +0000

    indev

---
+++ PythonLC/LaminariaDB/LaminariaDB.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+This module is distributed as part of the Laminaria Core (Python Version).
+Get the Source Code in GitHub:
+https://github.com/MrKelpy/LaminariaCore
+
+The LaminariaCore is Open Source and distributed under the
+MIT License
+"""
+
+# Built-in Imports
+from contextlib import suppress
+from typing import Dict, List, Any
+from pathlib import Path
+import string
+import os
+
+# Third Party Imports
+# Local Application Imports
+from exceptions import *
+from Collection import Collection
+
+
+class LaminariaDB:
+    """
+    Main class for the implementation of the Laminaria Database.
+    This is the only class that needs to ever be called at any point.
+    """
+
+    def __init__(self, path: str):
+        self._path: Path = Path(path)
+        self._cache: List[Dict[str, Any]] =list()
+        self.name: str = self._path.name
+        self.__prepare_database()
+        self.collections: Dict[str, Collection] = self.__get_collections()
+
+
+    def add_collection(self, name: str) -> Collection:
+        """
+        Creates a collection inside the database and prepares it into cache.
+
+        A collection is a portion of the database that contains
+        JSON documents with data with the aim of organising them
+        in a structured and clear way.
+
+        Two collections with the same name can't exist, and collection
+        names can only contain ASCII characters.
+        :return: Collection, new collection.
+        """
+
+        if name in os.listdir(self._path):
+            raise CollectionAlreadyExistsException(f"A collection named \"{name}\" already exists.")
+
+        # Checks if any x isn't present in the allowed characters for each letter in the name.
+        allowed_characters: str = string.ascii_letters + string.digits
+        if any(x not in allowed_characters for x in name):
+            raise InvalidCollectionNameException("Non-ASCII characters cannot be used to name collections.")
+
+        # Creates the new collection directory
+        collection_path: str = os.path.join(self._path, name)
+        os.makedirs(collection_path)
+
+        # Adds the new collection to the collections attribute and returns it
+        newcoll: Collection = Collection(collection_path, self._cache)
+        self.collections[name] = Collection(collection_path, self._cache)
+        return newcoll
+
+
+    def __prepare_database(self):
+        """
+        Prepares the database by creating any necessary folders
+        and doing any pre-usage operations.
+        :return:
+        """
+
+        # Creates the database folder if nonexistent
+        if not os.path.isdir(self._path):
+            os.makedirs(self._path)
+
+
+    def __get_collections(self) -> Dict[str, Collection]:
+        """
+        Returns all of the collections registered in the database.
+        Also prepares all of the collections into cache.
+        :return: Dict[str, Collection], a dictionary containing a key:val association for the collections.
+        """
+        collection_dict: Dict[str, Collection] = dict()
+
+        for collectdir in os.listdir(self._path):
+            collection_path: str = os.path.join(self._path, collectdir)
+            collection_dict[collectdir] = Collection(collection_path, self._cache)
+
+        return collection_dict
+
+
+db = LaminariaDB("./woeisme")
+with suppress(CollectionAlreadyExistsException):
+    db.add_collection("test")
+
+coll = db.collections["test"]
+coll.insert_many(
+    {"age":1, "name":"levi"},
+    {"age":3, "name":"levi2"},
+    {"age":2, "name":"lev3"},
+    {"age":3, "name":"levi4"},
+    {"age":3, "name":"levi5"},
+    {"age":3, "name":"levi"},
+    {"age":3, "name":"levi"},
+    {"age":2, "name":"levi"},
+    {"age":6, "name":"levi"},
+    {"age":3, "name":"levi"},
+    {"age":6, "name":"levi"},
+)
+disall = coll.find({"age":3})
+disall[0].update({"age":4, "name":"levi"})
+dis = coll.find({"name":"levi5"}, limit=1)[0]
+print(dis)
