421803c70e7d03753e0ff7becf4db6f6f3e4d273
MrKelpy <alexandresilva.coding@gmail.com>
2024-01-08 13:01:04 +0000

    Populated the fileutils further

--- C#/LaminariaCore-General/common/FileManager.cs
+++ C#/LaminariaCore-General/common/FileManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Threading.Tasks;
 using LaminariaCore_General.common.abstraction;
 using LaminariaCore_General.utils;
 
@@ -30,6 +31,30 @@ namespace LaminariaCore_General.common
             RootPath = OperationsTargetPath = PathUtils.NormalizePath(RootPath);
             
             FileUtils.EnsurePath(RootPath, FileAttributes.Directory);
+        }
+        
+        /// <summary>
+        /// Blocks the thread waiting for a file to be released by another process and returns
+        /// it as a FileStream when it eventually is.
+        /// </summary>
+        /// <param name="path">The path of the file to get</param>
+        /// <param name="timeout">A maximum timeout in milliseconds</param>
+        public static async Task<FileStream> WaitForFileAsync(string path, int timeout = 10000)
+        {
+            DateTime maxTimeout = DateTime.Now.AddMilliseconds(timeout);
+
+            while (DateTime.Now <= maxTimeout)
+            {
+                try
+                {
+                    // Tries to open the file, if it fails, continue the loop until the timeout is reached.
+                    Stream stream = new FileStream(path, FileMode.Open);
+                    return (FileStream)stream;
+                }
+                catch (IOException) { await Task.Delay(100); }
+            }
+
+            throw new TimeoutException("The file could not be accessed within the specified timeout.");
         }
     }
 }
